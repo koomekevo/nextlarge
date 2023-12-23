82e6a530eeb074f8d0a53f0d0b7044b51e22456e
koomekevo <koome.kevo@gurucrafts.net>
2023-12-23 17:01:20 +0300

    Add FontAwesomeIcon in DeleteBlock.jsx

--- app/(components)/DeleteBlock.jsx
+++ app/(components)/DeleteBlock.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import { faX } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const DeleteBlock = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <FontAwesomeIcon
+      icon={faX}
+      className=" text-red-400 hover:cursor-pointer hover:text-red-200"
+      onClick={deleteTicket}
+    />
+  );
+};
 
-export default DeleteBlock
+export default DeleteBlock;
